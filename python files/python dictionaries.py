655395468a94b92572f628d9c84313b53a953a63
Sowmya Kanithi <kanithisowmya2005@gmail.com>
2025-02-04 16:27:42 +0530

    Create python dictionaries.py

---
+++ python files/python dictionaries.py
@@ -0,0 +1,25 @@
+capitals = {"USA": "Washington D.C",
+            "India": "New Delhi",
+            "Vatikan city": "Vatikan city",
+            "Italy": "Rome"}
+'''print(dir(capitals))
+print(help(capitals))
+print(capitals.get("India"))
+if capitals.get("India"):
+    print("That capatal is exists")
+else:
+    print("Not exists")
+capitals.update({"france": "Paris"})
+print(capitals)
+capitals.pop("India")
+capitals.popitem()
+capitals.clear()
+keys = capitals.keys()
+for key in capitals.keys():
+    print(key)
+values = capitals.values()
+for value in capitals.values():
+    print(value)'''
+items = capitals.items()
+for key, value in capitals.items():
+    print(f"{key}: {value}")
